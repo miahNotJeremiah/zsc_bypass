163e3fdbf05fc856817bba159d502baabee6f7f3
Yirmeyah Ferrandiz <jerfer1@icloud.com>
2026-09-14 19:53:46 +1000

    Update simple_killer.bat

--- simple_killer.bat
+++ simple_killer.bat
@@ -1,6 +1,6 @@
 @echo off
 :loop
-//search for Task in Windows Task Manager
+#search for Task in Windows Task Manager
 for /f "tokens=1" %%i in ('tasklist ^| findstr "ZSA"') do (
     TASKKILL /F /IM %%i /T
 print("task killed")
